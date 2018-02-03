fefa58785b80c7e4eb05927ccc97c76d02ebb274
Aziza Larvet <aziza.larvet@etu.u-bordeaux.fr>
2018-02-03 19:09:47 +0100

    [ADD][CAMERA] Feature to use the camera
    A picture can be taken using the camera. It can then be modified.

--- SEGUIN_LARVET_AndroidProject/app/src/main/java/com/example/alarvet/seguin_larvet_androidproject/MainActivity.java
+++ SEGUIN_LARVET_AndroidProject/app/src/main/java/com/example/alarvet/seguin_larvet_androidproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,19 +21,40 @@ public class MainActivity extends AppCompatActivity {
 
     private Bitmap bitmap;
     private Bitmap originalBitmap;
-    private static int IMAGE_GALLERY_REQUEST = 1;
-    private ImageView picture;
+    private ImageView imageView;
     private Button galleryButton;
+    private Button cameraButton;
+    private static int IMAGE_GALLERY_REQUEST = 0;
+    private static int REQUEST_TAKE_PHOTO = 0;
 
 
     private View.OnClickListener galleryButtonListener = new View.OnClickListener() {
         public void onClick(View v) {
-            onImageGalleryClicked(v);
+            onImageGalleryClicked();
         }
     };
 
-    public void onImageGalleryClicked(View v) {
+    private View.OnClickListener cameraButtonListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            dispatchTakePictureIntent();
+        }
+    };
+
+    //static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            REQUEST_TAKE_PHOTO = 1;
+            IMAGE_GALLERY_REQUEST = 0;
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        }
+    }
+
+    public void onImageGalleryClicked() {
         Intent picturePickedIntent = new Intent(Intent.ACTION_PICK);
+        IMAGE_GALLERY_REQUEST = 1;
+        REQUEST_TAKE_PHOTO = 0;
 
         File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_ALARMS);
         String pictureDirectoryPath = pictureDirectory.getPath();
@@ -46,6 +68,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+
         if (resultCode == RESULT_OK) {
             //success
             if (requestCode == IMAGE_GALLERY_REQUEST) {
@@ -57,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                     inputStream = getContentResolver().openInputStream(imageUri);
                     bitmap = BitmapFactory.decodeStream(inputStream);
                     originalBitmap = BitmapFactory.decodeStream(inputStream);
-                    picture.setImageBitmap(bitmap);
+                    imageView.setImageBitmap(bitmap);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                     Toast.makeText(this, "Impossible to open the image", Toast.LENGTH_LONG).show();
@@ -72,9 +100,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        picture = (ImageView) findViewById(R.id.imageView);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         galleryButton = (Button) findViewById(R.id.galleryButton);
         galleryButton.setOnClickListener(galleryButtonListener);
+
+        cameraButton = (Button) findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(cameraButtonListener);
     }
 }
