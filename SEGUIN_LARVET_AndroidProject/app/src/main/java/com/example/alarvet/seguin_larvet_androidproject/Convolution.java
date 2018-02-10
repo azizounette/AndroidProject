c000d59de4bf7ae0939b55e8e13cde4a4ed56c22
Maxence <maxence.seguin.42@gmail.com>
2018-02-10 14:28:23 +0100

    [ADD] Added effects
    Added in convolutions function in convolution class. Also added all the tools to make it works.

--- SEGUIN_LARVET_AndroidProject/app/src/main/java/com/example/alarvet/seguin_larvet_androidproject/Convolution.java
+++ SEGUIN_LARVET_AndroidProject/app/src/main/java/com/example/alarvet/seguin_larvet_androidproject/Convolution.java
@@ -58,6 +58,7 @@ public class Convolution extends Filter {
 
         int[] pixels = new int[n];
         bmp.getPixels(pixels, 0, w, 0, 0, w, h);
+
         int[] SATR  = (int[]) summedAreaTable(pixels, w, h)[0];
         int[] SATG  = (int[]) summedAreaTable(pixels, w, h)[1];
         int[] SATB  = (int[]) summedAreaTable(pixels, w, h)[2];
@@ -72,6 +73,80 @@ public class Convolution extends Filter {
         bmp.setPixels(pixels, 0, w,  0, 0, w, h);
     }
     /* End of average blurring effect */
+
+    private float gauss (int x, int y, double sigma, int radius) {
+        return (float) (10*radius*Math.exp(-((y-radius)*(y-radius)+(x-radius)*(x-radius))/(2*sigma*sigma)));
+    }
+
+    // 1 = AverageBlur    2 = GaussianBlur    0/3 = Contouring    4 = Sharpening
+    private int[] ConvolutionMatrix (int cases , int dimension) {
+        int[] res = new int[dimension*dimension];
+        switch (cases) {
+            case 0:
+                res[0] = -1;res[2] = 1;res[3] = -1;res[5] = 1;res[6] = -1;res[8] = 1;
+                break;
+            case 1:
+                for (int i = 0; i < dimension*dimension; i++) {
+                    res[i] = 1;
+                }
+                break;
+            case 2:
+                int radius = (dimension - 1)/2;
+                double sigma = Math.sqrt(radius*radius/(Math.log(10*radius)));
+                for (int m = 0; m < dimension; m++) {
+                    for (int n = 0; n < dimension; n++) {
+                        res[m + dimension * n] = (int) gauss(m, n, sigma, radius);
+                    }
+                }
+                break;
+            case 3:
+                res[0] = -1;res[1] = -2;res[2] = -1;res[6] = 1;res[7] = 2;res[8] = 1;
+                break;
+            case 4:
+                res[1] = -1; res[3] = -1; res[4] = 5; res[5] = -1; res[7] = -1;
+                break;
+        }
+        return res;
+    }
+
+    public void convolutions (int[] matrixConvo) {
+        Bitmap bmp = this.getBmp();
+        int w = bmp.getWidth();
+        int h = bmp.getHeight();
+        int n = w * h;
+
+        int[] pixels = new int[n];
+        int[] pixelsRef = new int[n];
+        bmp.getPixels(pixels, 0, w, 0, 0, w, h);
+
+        int matrixWidth = (int) Math.sqrt(matrixConvo.length);
+        int radius = ((matrixWidth-1)/2);
+
+        float R;
+        float G;
+        float B;
+        int weight = 0;
+        for (int i = 0; i < matrixWidth*matrixWidth; i++) {
+            weight += matrixConvo[i];
+        }
+
+        for (int k = radius; k < w - radius; k++) {
+            for (int l = radius; l < h - radius; l++) {
+                R = 0;
+                G = 0;
+                B = 0;
+                for (int m = 0; m < matrixWidth; m++) {
+                    for (int o = 0; o < matrixWidth; o++) {
+                        R += Color.red(pixelsRef[k - radius + o + (l - radius + m) * w]) * matrixConvo[o + m * matrixWidth];
+                        G += Color.green(pixelsRef[k - radius + o + (l - radius + m) * w]) * matrixConvo[o + m * matrixWidth];
+                        B += Color.blue(pixelsRef[k - radius + o + (l - radius + m) * w]) * matrixConvo[o + m * matrixWidth];
+                    }
+                }
+                pixels[k + l * w] = Color.rgb((int) R / weight, (int) G / weight, (int) B / weight);
+            }
+        }
+        bmp.setPixels(pixels, 0, w,  0, 0, w, h);
+    }
 
 
 }
